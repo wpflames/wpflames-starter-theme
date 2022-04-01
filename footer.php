6d63340024eff0c776c4bd4e43a3e4c1d017fa17
wpflames <info@wpflames.hu>
2022-04-01 16:57:30 +0200

    Structural changes

--- footer.php
+++ footer.php
@@ -1,36 +1,52 @@
 <?php
 /**
  * The template for displaying the footer
- *
- * Contains the closing of the #content div and all content after.
- *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
- * @package Underscore
  */
 
-// Exit if accessed directly.
-defined( 'ABSPATH' ) || exit; 
-?>
+defined( 'ABSPATH' ) || exit; ?>
 
-	<footer id="colophon" class="site-footer">
-		<div class="wrap">
-			<div class="site-info">
-				<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'underscore' ) ); ?>">
-					<?php
-					/* translators: %s: CMS name, i.e. WordPress. */
-					printf( esc_html__( 'Proudly powered by %s', 'underscore' ), 'WordPress' );
-					?>
-				</a>
-				<span class="sep"> | </span>
-					<?php
-					/* translators: 1: Theme name, 2: Theme author. */
-					printf( esc_html__( 'Theme: %1$s by %2$s.', 'underscore' ), 'underscore', '<a href="http://underscores.me/">Underscores.me</a>' );
-					?>
-			</div><!-- .site-info -->
+	<footer class="site-footer">
+		<div class="site-info">
+				<div class="wrap">
+				<div class="grid grid-3">
+					<div class="grid-item">
+						<h4>About</h4>
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae neque ducimus repellat optio assumenda culpa nostrum beatae! Unde beatae quam neque. Dicta repellat repellendus atque consequuntur, cumque minima dolore pariatur iusto eveniet rerum labore esse hic facilis. Deserunt error impedit temporibus doloribus est nesciunt quisquam beatae modi, aliquam quaerat, unde doloremque odit? Tempore perspiciatis amet, ipsa reiciendis, quasi, repellendus quas quos neque ex tempora dolorum! Maxime excepturi numquam, molestiae, eaque nam ullam officiis deleniti quasi soluta, quibusdam atque impedit. Numquam quia laborum et sapiente commodi! Odio fugit id nisi voluptatem et totam non reiciendis, distinctio nostrum error corrupti numquam eos.</p>
+					</div>
+					<div class="grid-item">
+						<h4>Popular posts</h4>
+						<ul>
+							<li><a href="#">Lorem ipsum dolor sit amet</a></li>
+							<li><a href="#">Lorem ipsum dolor sit amet</a></li>
+							<li><a href="#">Lorem ipsum dolor sit amet</a></li>
+							<li><a href="#">Lorem ipsum dolor sit amet</a></li>
+							<li><a href="#">Lorem ipsum dolor sit amet</a></li>
+							<li><a href="#">Lorem ipsum dolor sit amet</a></li>
+						</ul>
+					</div>
+					<div class="grid-item">
+						<h4>Other informations</h4>
+						<ul>
+							<li><a href="#">Lorem ipsum dolor sit amet</a></li>
+							<li><a href="#">Lorem ipsum dolor sit amet</a></li>
+							<li><a href="#">Lorem ipsum dolor sit amet</a></li>
+							<li><a href="#">Lorem ipsum dolor sit amet</a></li>
+							<li><a href="#">Lorem ipsum dolor sit amet</a></li>
+							<li><a href="#">Lorem ipsum dolor sit amet</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
 		</div>
-	</footer><!-- #colophon -->
-</div><!-- #page -->
+			<div class="privacy">
+				<a target="_blank" href="">Privacy Policy</a>
+			</div>
+			<div class="copyright">
+				&copy; <?php echo date('Y'); ?> · Company Name
+			</div>
+		
+	</footer>
+</div>
 
 <?php wp_footer(); ?>
 
